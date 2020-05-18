7ffcd8a92588cf390721f0552c5034ecbc50a95d
Nick <naf.integra@gmail.com>
2020-05-18 00:48:07 -0500

    Updates

--- physics.js
+++ physics.js
@@ -1,1 +1,17 @@
-gsap.to(".circle", {duration: 1, x: 100, repeat: 1, yoyo:true});
+document.getElementsByClassName("circle");
+document.addEventListener('animationiteration')
+let y = 51;
+
+function myAnimation(){
+    gsap.to(".circle", {duration: 1, scale: 1, repeat: 1, yoyo:true});
+}     
+
+
+function replayAnimation() {
+    if (x <= 51 && y <= 51 ) {
+        myAnimation();
+    }
+
+}
+
+replayAnimation();
