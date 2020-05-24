d0897ddad581d3d6aaf907e0bc5a47d62ee75dd1
Nick <naf.integra@gmail.com>
2020-05-24 08:47:40 -0500

    update

--- physics.js
+++ physics.js
@@ -1,7 +1,13 @@
 const squares =document.querySelectorAll('.squares');
 
+// const object = {
+//     percentage: 0%,
+//     number:0
+// }
+
 anime({
     targets: 'squares',
+//    percentage: '150%',
 /* ['#squares', '#squares'] | '#squares, #squares' */
 
     translateX: 500
