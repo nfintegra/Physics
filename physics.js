cddc68a1ce340262b12eb67729cf93e7199129c7
Nick <naf.integra@gmail.com>
2020-05-24 10:53:31 -0500

    update

--- physics.js
+++ physics.js
@@ -1,4 +1,4 @@
-// const squares = document.querySelectorAll('.squares');
+// const squares = document#querySelectorAll('#squares');
 
 // // const object = {
 // //     percentage: 0%,
@@ -14,48 +14,54 @@
   
 // });
 
+anime({ 
+        targets: '.square',
+        translateX: 270,
+        easing: 'easInOutSine',
+    });
+
 anime({
-    targets: '.loop',
-    translateX: 270,
+    targets: '#loop',
+    // translateX: 270,
     loop: 3,
-    easing: 'easeInOutSine'
+    // easing: 'easeInOutSine'
   });
   
   anime({
-    targets: '.loop-infinity',
-    translateX: 270,
+    targets: '#loop-infinity',
+    // translateX: 270,
     loop: true,
-    easing: 'easeInOutSine'
+    // easing: 'easeInOutSine'
   });
   
   anime({
-    targets: '.loop-reverse',
-    translateX: 270,
+    targets: '#loop-reverse',
+    // translateX: 270,
     loop: 3,
     direction: 'reverse',
-    easing: 'easeInOutSine'
+    // easing: 'easeInOutSine'
   });
   
   anime({
-    targets: '.loop-reverse-infinity',
-    translateX: 270,
+    targets: '#loop-reverse-infinity',
+    // translateX: 270,
     direction: 'reverse',
     loop: true,
-    easing: 'easeInOutSine'
+    // easing: 'easeInOutSine'
   });
   
   anime({
-    targets: '.loop-alternate',
-    translateX: 270,
+    targets: '#loop-alternate',
+    // translateX: 270,
     loop: 3,
     direction: 'alternate',
-    easing: 'easeInOutSine'
+    // easing: 'easeInOutSine'
   });
   
   anime({
-    targets: '.loop-alternate-infinity',
-    translateX: 270,
+    targets: '#loop-alternate-infinity',
+    // translateX: 270,
     direction: 'alternate',
     loop: true,
-    easing: 'easeInOutSine'
+    // easing: 'easeInOutSine'
   });
