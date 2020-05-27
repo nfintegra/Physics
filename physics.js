ae82614467f0832d57ab185074c57f297c6f5638
Nick <naf.integra@gmail.com>
2020-05-27 00:53:58 -0500

    Up

--- physics.js
+++ physics.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded',
   })
   .add({
     targets: '.welcome_text',
-    top: ['-150%','50%'],
+    top: ['-100%','50%'],
     easing: 'easeOutQuad',
     offset: '-=600',
     duration: '700',
