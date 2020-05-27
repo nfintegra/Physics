d43e8a97f98eb98a3080cc3014de4092c40b182f
Nick <naf.integra@gmail.com>
2020-05-26 23:48:15 -0500

    update

--- physics.js
+++ physics.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', 
-()=>{anime.timeline({})
+()=>{
+  anime.timeline({
+
+  })
   .add({
     targets: '.cover',
     height: ['60%','200%'],
@@ -9,14 +12,14 @@ document.addEventListener('DOMContentLoaded',
   })
   .add({
     targets: 'welcome_text h2',
-    top: ['-150%','-50%'],
+    top: ['-150%','50%'],
     easing: 'easeOutQuad',
     offset: '-=600',
     duration: '700',
   })
   .add({
     targets: 'welcome_text h2',
-    top: ['-50%','-150%'],
+    top: ['50%','-150%'],
     easing: 'easeOutQuad',
     offset: '-=1000',
     duration: '700',
