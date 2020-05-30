062a03ee558e1f238356ef54163ee8c4811ed6bf
Nick <naf.integra@gmail.com>
2020-05-29 23:01:03 -0500

    update

--- physics.js
+++ physics.js
@@ -18,4 +18,29 @@ document.addEventListener('DOMContentLoaded',
     duration: '700',
   })
 
+  .add({
+    targets: '.welcome_text h2',
+    top : ['50%', '150%'],
+    easing: 'easeOutQuad',
+    offest: '-=400',
+    duration: '600',
+  })
+
+  .add({
+    targets: '.cover',
+    height: ['60%','200%'],
+    top: ['-90%', '100%'],
+    easing: 'easeOutCubic',
+    duration: '1200',
+    complete: (anime)=> {
+      document.querySelector('.welcome_text')
+      .style.display='none';
+
+      document.querySelector('.main_content')
+      .style.display='block';
+
+    }
+  })
+
+
 })
